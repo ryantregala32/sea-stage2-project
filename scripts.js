9f9c318c520cd636e6068b37fc98073c762ab04d
ryantregala32 <133082563+ryantregala32@users.noreply.github.com>
2024-04-09 19:34:52 -0700

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -24,71 +24,147 @@
  */
 
 
-const FRESH_PRINCE_URL = "https://upload.wikimedia.org/wikipedia/en/3/33/Fresh_Prince_S1_DVD.jpg";
-const CURB_POSTER_URL = "https://m.media-amazon.com/images/M/MV5BZDY1ZGM4OGItMWMyNS00MDAyLWE2Y2MtZTFhMTU0MGI5ZDFlXkEyXkFqcGdeQXVyMDc5ODIzMw@@._V1_FMjpg_UX1000_.jpg";
-const EAST_LOS_HIGH_POSTER_URL = "https://static.wikia.nocookie.net/hulu/images/6/64/East_Los_High.jpg";
+// const FRESH_PRINCE_URL = "https://upload.wikimedia.org/wikipedia/en/3/33/Fresh_Prince_S1_DVD.jpg";
+// const CURB_POSTER_URL = "https://m.media-amazon.com/images/M/MV5BZDY1ZGM4OGItMWMyNS00MDAyLWE2Y2MtZTFhMTU0MGI5ZDFlXkEyXkFqcGdeQXVyMDc5ODIzMw@@._V1_FMjpg_UX1000_.jpg";
+// const EAST_LOS_HIGH_POSTER_URL = "https://static.wikia.nocookie.net/hulu/images/6/64/East_Los_High.jpg";
 
-// This is an array of strings (TV show titles)
-let titles = [
-    "Fresh Prince of Bel Air",
-    "Curb Your Enthusiasm",
-    "East Los High"
+// // This is an array of strings (TV show titles)
+// let titles = [
+//     "Fresh Prince of Bel Air",
+//     "Curb Your Enthusiasm",
+//     "East Los High"
+// ];
+// // Your final submission should have much more data than this, and 
+// // you should use more than just an array of strings to store it all.
+
+
+// // This function adds cards the page to display the data in the array
+// function showCards() {
+//     const cardContainer = document.getElementById("card-container");
+//     cardContainer.innerHTML = "";
+//     const templateCard = document.querySelector(".card");
+    
+//     for (let i = 0; i < titles.length; i++) {
+//         let title = titles[i];
+
+//         // This part of the code doesn't scale very well! After you add your
+//         // own data, you'll need to do something totally different here.
+//         let imageURL = "";
+//         if (i == 0) {
+//             imageURL = FRESH_PRINCE_URL;
+//         } else if (i == 1) {
+//             imageURL = CURB_POSTER_URL;
+//         } else if (i == 2) {
+//             imageURL = EAST_LOS_HIGH_POSTER_URL;
+//         }
+
+//         const nextCard = templateCard.cloneNode(true); // Copy the template card
+//         editCardContent(nextCard, title, imageURL); // Edit title and image
+//         cardContainer.appendChild(nextCard); // Add new card to the container
+//     }
+// }
+
+// function editCardContent(card, newTitle, newImageURL) {
+//     card.style.display = "block";
+
+//     const cardHeader = card.querySelector("h2");
+//     cardHeader.textContent = newTitle;
+
+//     const cardImage = card.querySelector("img");
+//     cardImage.src = newImageURL;
+//     cardImage.alt = newTitle + " Poster";
+
+//     // You can use console.log to help you debug!
+//     // View the output by right clicking on your website,
+//     // select "Inspect", then click on the "Console" tab
+//     console.log("new card:", newTitle, "- html: ", card);
+// }
+
+// // This calls the addCards() function when the page is first loaded
+// document.addEventListener("DOMContentLoaded", showCards);
+
+// function quoteAlert() {
+//     console.log("Button Clicked!")
+//     alert("I guess I can kiss heaven goodbye, because it got to be a sin to look this good!");
+// }
+
+// function removeLastCard() {
+//     titles.pop(); // Remove last item in titles array
+//     showCards(); // Call showCards again to refresh
+// }
+let catalog = [
+    { name: "Yamaha F325D Dreadnought Acoustic Guitar", category: "guitar", price: 199.99, imageURL: "https://m.media-amazon.com/images/I/61RLzFHpZNL._AC_SL1500_.jpg" },
+    { name: "Fender Stratocaster Electric Guitar", category: "guitar", price: 799.99, imageURL: "https://media.guitarcenter.com/is/image/MMGS7/M08143000001000-02-600x600.jpg" },
+    { name: "Epiphone 60's Standard Les Paul Guitar - Iced Tea", category: "guitar", price: 699.00, imageURL: "https://i.imgur.com/nJHz9qv_d.webp?maxwidth=1520&fidelity=grand" },
+    { name: "Squier 40th Anniversary Precision Bass Guitar Vintage Edition Satin Dakota Red", category: "bass", price: 399.99, imageURL: "https://media.guitarcenter.com/is/image/MMGS7/L92325000002000-02-600x600.jpg" },
+    { name: "Fender American Ultra Jazz Electric Bass", category: "bass", price: 2299.99, imageURL: "https://cf1.zzounds.com/media/productmedia/fit,600by800/quality,85/1_Full_Straight_Front_NA-97f4566378d0b62870350aeaaee0b0a5.jpg" }
 ];
-// Your final submission should have much more data than this, and 
-// you should use more than just an array of strings to store it all.
 
-
-// This function adds cards the page to display the data in the array
-function showCards() {
+// Function to display catalog items
+function showCatalog() {
     const cardContainer = document.getElementById("card-container");
     cardContainer.innerHTML = "";
-    const templateCard = document.querySelector(".card");
+
+    catalog.forEach(item => {
+        const nextCard = createCard(item);
+        cardContainer.appendChild(nextCard);
+    });
+}
+
+// Function to create a card for a catalog item
+function createCard(item) {
+    const card = document.createElement("div");
+    card.classList.add("card");
+
+    const cardContent = document.createElement("div");
+    cardContent.classList.add("card-content");
+
+    const title = document.createElement("h2");
+    title.textContent = item.name;
+
+    const image = document.createElement("img");
+    image.src = item.imageURL;
+    image.alt = item.name + " Image";
+
+    const price = document.createElement("p");
+    price.textContent = "Price: $" + item.price.toFixed(2);
+
+    const category = document.createElement("p");
+    category.textContent = "Category: " + item.category;
+
+    cardContent.appendChild(title);
+    cardContent.appendChild(image);
+    cardContent.appendChild(price);
+    cardContent.appendChild(category);
+
+    card.appendChild(cardContent);
+
+    return card;
+}
+
+// This calls the showCatalog() function when the page is first loaded
+document.addEventListener("DOMContentLoaded", showCatalog);
+
+function searchByName() {
+    // Get the search input value
+    const searchInput = document.getElementById('search-input').value.toLowerCase();
     
-    for (let i = 0; i < titles.length; i++) {
-        let title = titles[i];
-
-        // This part of the code doesn't scale very well! After you add your
-        // own data, you'll need to do something totally different here.
-        let imageURL = "";
-        if (i == 0) {
-            imageURL = FRESH_PRINCE_URL;
-        } else if (i == 1) {
-            imageURL = CURB_POSTER_URL;
-        } else if (i == 2) {
-            imageURL = EAST_LOS_HIGH_POSTER_URL;
-        }
-
-        const nextCard = templateCard.cloneNode(true); // Copy the template card
-        editCardContent(nextCard, title, imageURL); // Edit title and image
-        cardContainer.appendChild(nextCard); // Add new card to the container
+    // Filter the catalog based on the search input
+    const searchResults = catalog.filter(item => item.name.toLowerCase().includes(searchInput));
+    
+    // Display the search results
+    if (searchResults.length > 0) {
+        displayCatalog(searchResults);
+    } else {
+        alert('No items found matching your search.');
     }
 }
 
-function editCardContent(card, newTitle, newImageURL) {
-    card.style.display = "block";
-
-    const cardHeader = card.querySelector("h2");
-    cardHeader.textContent = newTitle;
-
-    const cardImage = card.querySelector("img");
-    cardImage.src = newImageURL;
-    cardImage.alt = newTitle + " Poster";
-
-    // You can use console.log to help you debug!
-    // View the output by right clicking on your website,
-    // select "Inspect", then click on the "Console" tab
-    console.log("new card:", newTitle, "- html: ", card);
+// Function to remove the corresponding card from the catalog
+function removeItem(button) {
+    const card = button.closest('.card');
+    const index = Array.from(card.parentNode.children).indexOf(card);
+    catalog.splice(index, 1);
+    showCatalog();
 }
 
-// This calls the addCards() function when the page is first loaded
-document.addEventListener("DOMContentLoaded", showCards);
-
-function quoteAlert() {
-    console.log("Button Clicked!")
-    alert("I guess I can kiss heaven goodbye, because it got to be a sin to look this good!");
-}
-
-function removeLastCard() {
-    titles.pop(); // Remove last item in titles array
-    showCards(); // Call showCards again to refresh
-}
